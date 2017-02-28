d421fd80f450c9653ff6e62e17556dbd1fd03bca
Michał Przyszczypkowski <michal@unit9.com>
2017-02-28 16:58:36 +0100

    Deploy config

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-/*eslint no-console:0 */
-'use strict';
-require('core-js/fn/object/assign');
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config');
-const open = require('open');
+const express = require('express');
+const app = express();
 
-new WebpackDevServer(webpack(config), config.devServer)
-.listen(config.port, 'localhost', (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Listening at localhost:' + config.port);
-  console.log('Opening your system browser...');
-  open('http://localhost:' + config.port + '/webpack-dev-server/');
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/dist/index.html');
 });
+
+app.listen(process.env.PORT || 3000);
